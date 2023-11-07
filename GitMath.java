3f6e0ad71fb84e7443ec25eaf6325d2ceba3f6a6
Danielle Huser <78627684+daniellehuser@users.noreply.github.com>
2023-11-06 21:48:18 -0500

    Multiplication Method
    The original program

---
+++ GitMath.java
@@ -0,0 +1,13 @@
+package GitAssignment;
+
+
+public class GitMath {
+
+    public static int multiply(int num1, int num2) {
+        int result = num1 * num2;
+        System.out.println("The product is: " + result);
+        return result;
+
+    }
+
+}
